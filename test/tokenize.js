404c21c91937454110d41cd4a02b55190ba74d74
达峰 <dafeng.xdf@taobao.com>
2013-11-15 14:41:56 +0800

    add test case

---
+++ test/tokenize.js
@@ -0,0 +1,43 @@
+"use strict";
+var fs = require('fs');
+var should = require('should');
+
+var passme = require('../bin/passme');
+
+describe('BooleanLiteral', function(){
+    var code = 'var a = !true,false;';
+    it('should return array when the value is BooleanLiteral', function(){
+        passme.tokenize(code).length.should.equal(8);
+    })
+})
+describe('RegularExpression', function(){
+    var reg = '/^(?:(?:or|and|like)\s*)+|\s*(?:(?:or|and|like)\s*)+$/mi;';
+    var code = 'a = '+reg;
+    it('should return array when the value is RegularExpression', function(){
+        passme.tokenize(code)[2].value.should.equal('/^(?:(?:or|and|like)s*)+|s*(?:(?:or|and|like)s*)+$/mi');
+    })
+})
+describe('Punctuator', function(){
+    var code ='a /=(b/(c/(d/(e*f))))';
+    it('should return array when the value is Punctuator', function(){
+        passme.tokenize(code)[1].value.should.equal('/=');
+    })
+    it('should return array when the value is Punctuator', function(){
+        passme.tokenize(code).length.should.equal(19);
+    })
+    it('should return array when the value is Punctuator', function(){
+        passme.tokenize(code)[13].type.should.equal('Punctuator');
+    })
+})
+describe('jQuery', function(){
+    var jQuery = fs.readFileSync('test/3rdparty/jquery-1.10.2.js','utf8');
+    it('should return array when the value is jQuery', function(){
+        passme.tokenize(jQuery).length.should.equal(48007);
+    })
+})
+describe('kissy', function(){
+    var kissy = fs.readFileSync('test/3rdparty/kissy-1.4.0.js','utf8');
+    it('should return array when the value is jQuery', function(){
+        passme.tokenize(kissy)[22811].value.should.equal('nodejs');
+    })
+})
