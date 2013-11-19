59522b05ead7f0e5022d46bc406ba710cbdc5674
达峰 <dafeng.xdf@taobao.com>
2013-11-19 17:24:11 +0800

    bugfix &&

--- bin/passme.js
+++ bin/passme.js
@@ -2,7 +2,7 @@
  * passme.js v1.0.3
  *
  * parse me!
- * Latest build : 2013-11-19 17:20:07
+ * Latest build : 2013-11-19 17:23:53
  *
  * ================================================================
  * * Copyright (C) 2012-2013 xudafeng <xudafeng@126.com>
@@ -309,7 +309,7 @@
                 var isReg = true;
                 for(var i=that.tokens.length;i>=0;i--){
                     if(that.tokens[i] && that.tokens[i].type!=='WhiteSpace'){
-                        if(that.tokens[i].type =='Punctuator' && (that.tokens[i].value =='||' || that.tokens[i].value =='='||that.tokens[i].value =='!'||that.tokens[i].value =='['||that.tokens[i].value =='('||that.tokens[i].value==':')||that.tokens[i].type =='Keyword'){
+                        if(that.tokens[i].type =='Punctuator' && (that.tokens[i].value =='||' || that.tokens[i].value =='&&' || that.tokens[i].value =='='||that.tokens[i].value =='!'||that.tokens[i].value =='['||that.tokens[i].value =='('||that.tokens[i].value==':')||that.tokens[i].type =='Keyword'){
                             isReg = true;
                         }else {
                             isReg = false;
